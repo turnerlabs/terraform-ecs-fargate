887aae928a510606c181849718712df236003e3d
James Hodnett <jhodnett@jamess-mbp.turner.com>
2018-08-09 09:15:17 -0400

    adding custom KMS key addition to the secretsmanger secret

--- env/dev/secretsmanager.tf
+++ env/dev/secretsmanager.tf
@@ -1,17 +1,44 @@
-# create the secretsmanager secret
-resource "aws_secretsmanager_secret" "sm_secret" {
-  name   = "${var.app}-${var.environment}"
-  tags   = "${var.tags}"
-  policy = "${data.aws_iam_policy_document.sm_resource_policy_doc.json}"
-}
-
-# get the saml user info so we can get the unique_id
-data "aws_iam_role" "saml_role" {
-  name = "${var.saml_role}"
-}
-
 # Allow for code reuse...
 locals {
+  # KMS write actions
+  kms_write_actions = [
+    "kms:CancelKeyDeletion",
+    "kms:CreateAlias",
+    "kms:CreateGrant",
+    "kms:CreateKey",
+    "kms:DeleteAlias",
+    "kms:DeleteImportedKeyMaterial",
+    "kms:DisableKey",
+    "kms:DisableKeyRotation",
+    "kms:EnableKey",
+    "kms:EnableKeyRotation",
+    "kms:Encrypt",
+    "kms:GenerateDataKey",
+    "kms:GenerateDataKeyWithoutPlaintext",
+    "kms:GenerateRandom",
+    "kms:GetKeyPolicy",
+    "kms:GetKeyRotationStatus",
+    "kms:GetParametersForImport",
+    "kms:ImportKeyMaterial",
+    "kms:PutKeyPolicy",
+    "kms:ReEncryptFrom",
+    "kms:ReEncryptTo",
+    "kms:RetireGrant",
+    "kms:RevokeGrant",
+    "kms:ScheduleKeyDeletion",
+    "kms:TagResource",
+    "kms:UntagResource",
+    "kms:UpdateAlias",
+    "kms:UpdateKeyDescription",
+  ]
+
+  # KMS read actions
+  kms_read_actions = [
+    "kms:Decrypt",
+    "kms:DescribeKey",
+    "kms:List*",
+  ]
+
   # secretsmanager write actions
   sm_write_actions = [
     "secretsmanager:CancelRotateSecret",
@@ -48,6 +75,111 @@ locals {
   ]
 
   sm_arn = "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:${var.app}-${var.environment}-??????"
+}
+
+# create the KMS key for this secret
+resource "aws_kms_key" "sm_kms_key" {
+  description = "${var.app}-${var.environment}"
+  policy      = "${data.aws_iam_policy_document.kms_resource_policy_doc.json}"
+  tags        = "${merge(var.tags, map("Name", format("%s-%s", var.app, var.environment)))}"
+}
+
+# alias for the key
+resource "aws_kms_alias" "sm_kms_alias" {
+  name          = "alias/${var.app}-${var.environment}"
+  target_key_id = "${aws_kms_key.sm_kms_key.key_id}"
+}
+
+# the kms key policy
+data "aws_iam_policy_document" "kms_resource_policy_doc" {
+  statement = {
+    sid    = "DenyWriteToAllExceptSAMLUsers"
+    effect = "Deny"
+
+    principals = {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions   = ["${local.kms_write_actions}"]
+    resources = ["*"]
+
+    condition = {
+      test     = "StringNotLike"
+      variable = "aws:userId"
+      values   = ["${local.saml_user_ids}"]
+    }
+  }
+
+  statement = {
+    sid    = "DenyReadToAllExceptRoleAndSAMLUsers"
+    effect = "Deny"
+
+    principals = {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions   = ["${local.kms_read_actions}"]
+    resources = ["*"]
+
+    condition = {
+      test     = "StringNotLike"
+      variable = "aws:userId"
+      values   = ["${local.role_and_saml_ids}"]
+    }
+  }
+
+  statement = {
+    sid    = "AllowWriteToSAMLUsers"
+    effect = "Allow"
+
+    principals = {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions   = ["${local.kms_write_actions}"]
+    resources = ["*"]
+
+    condition = {
+      test     = "StringLike"
+      variable = "aws:userId"
+      values   = ["${local.saml_user_ids}"]
+    }
+  }
+
+  statement = {
+    sid    = "AllowReadRoleAndSAMLUsers"
+    effect = "Allow"
+
+    principals = {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions   = ["${local.kms_read_actions}"]
+    resources = ["*"]
+
+    condition = {
+      test     = "StringLike"
+      variable = "aws:userId"
+      values   = ["${local.role_and_saml_ids}"]
+    }
+  }
+}
+
+# create the secretsmanager secret
+resource "aws_secretsmanager_secret" "sm_secret" {
+  name       = "${var.app}-${var.environment}"
+  kms_key_id = "${aws_kms_key.sm_kms_key.key_id}"
+  tags       = "${var.tags}"
+  policy     = "${data.aws_iam_policy_document.sm_resource_policy_doc.json}"
+}
+
+# get the saml user info so we can get the unique_id
+data "aws_iam_role" "saml_role" {
+  name = "${var.saml_role}"
 }
 
 # resource policy doc that limits access to secret
