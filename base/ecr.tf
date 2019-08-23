947539c96e30d81417fd6cd7ae3bdac3dfa311e1
John Ritsema <john.ritsema@turner.com>
2019-08-23 10:52:23 -0400

    defaults ecr to immutable

--- base/ecr.tf
+++ base/ecr.tf
@@ -4,9 +4,17 @@
  * https://aws.amazon.com/ecr/
  */
 
+# The tag mutability setting for the repository (defaults to IMMUTABLE)
+variable "image_tag_mutability" {
+  type        = string
+  default     = "IMMUTABLE"
+  description = "The tag mutability setting for the repository (defaults to IMMUTABLE)"
+}
+
 # create an ECR repo at the app/image level
 resource "aws_ecr_repository" "app" {
-  name = var.app
+  name                 = var.app
+  image_tag_mutability = var.image_tag_mutability
 }
 
 data "aws_caller_identity" "current" {
