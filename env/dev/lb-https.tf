acf3361521e30d7800caee92a57fde856c510d88
John Ritsema <john.ritsema@turner.com>
2020-10-06 13:39:53 -0400

    updates lb-https.tf to tf 12+ syntax

--- env/dev/lb-https.tf
+++ env/dev/lb-https.tf
@@ -10,13 +10,13 @@ variable "https_port" {
 variable "certificate_arn" {}
 
 resource "aws_alb_listener" "https" {
-  load_balancer_arn = "${aws_alb.main.id}"
-  port              = "${var.https_port}"
+  load_balancer_arn = aws_alb.main.id
+  port              = var.https_port
   protocol          = "HTTPS"
-  certificate_arn   = "${var.certificate_arn}"
+  certificate_arn   = var.certificate_arn
 
   default_action {
-    target_group_arn = "${aws_alb_target_group.main.id}"
+    target_group_arn = aws_alb_target_group.main.id
     type             = "forward"
   }
 }
@@ -24,9 +24,9 @@ resource "aws_alb_listener" "https" {
 resource "aws_security_group_rule" "ingress_lb_https" {
   type              = "ingress"
   description       = "HTTPS"
-  from_port         = "${var.https_port}"
-  to_port           = "${var.https_port}"
+  from_port         = var.https_port
+  to_port           = var.https_port
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.nsg_lb.id}"
+  security_group_id = aws_security_group.nsg_lb.id
 }
