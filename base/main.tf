1cb269d0afd9748505b1ab3285764a3bc89643a8
John Ritsema <john_ritsema@yahoo.com>
2022-05-13 18:10:53 -0400

    adds required_providers to pin aws provider to v4

--- base/main.tf
+++ base/main.tf
@@ -1,5 +1,12 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0, < 5.0.0"
+    }
+  }
 }
 
 /**
